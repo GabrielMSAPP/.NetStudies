65a8d45c2a0b768bcf3b4d04e39920a67a7ac070
Gabriel Souza <69609505+GabrielSouza99@users.noreply.github.com>
2022-01-31 22:02:44 -0300

    Primeiro projeto com .net

---
+++ PrimeiroProjeto/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using projeto.entities;
+
+namespace projeto
+{
+    public class PrimeiroProjeto
+    {
+        static void Main(String[] args)
+        {
+            Heroi h1 = new Heroi("Gabriel", 100, 100, 100);
+            HeroiTerra h2 = new HeroiTerra("Paulo", 100, 75, 75);
+
+            Console.WriteLine(h1.atacar());
+            Console.WriteLine(h2.atacar());
+            Console.WriteLine(h1.apresentar());
+        }
+
+    }
+
+}
